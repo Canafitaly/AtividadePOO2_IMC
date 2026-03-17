9d2a1da67bef4c910cf8cd250a5ddd7f34f67b97
pedro.machado <pedrohmachado14@gmail.com>
2026-03-17 15:23:07 -0300

    Pronto, agora consegue empilhar dados

--- src/main/java/org/model/Arquivo.java
+++ src/main/java/org/model/Arquivo.java
@@ -1,18 +1,42 @@
 package org.model;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arquivo {
-    Path caminhodoarquivo;
-    public Arquivo(String nomeArquivo,String nome,String saude,double peso,double altura,double imc, int idade ) throws IOException {
-        caminhodoarquivo= Paths.get(nomeArquivo);
-        String texto=escrever(nome,saude,peso,altura,imc,idade);
-        Files.writeString(caminhodoarquivo,texto);
+    public Path caminhodoarquivo;
+    public String campos = "nome; saude; peso; altura; imc; idade";
+
+    public Arquivo(String nomeArquivo, String nome, String saude, double peso, double altura, double imc, int idade) throws IOException {
+        //criar caso não exista o arquivo
+        caminhodoarquivo = Paths.get(nomeArquivo);
+
+        //salvando o progresso e vendo se as colunas existem
+        List<String> gravados=verificandoSalvos();
+
+        // Adiciona novo registro
+        String texto = organizar(nome, saude, peso, altura, imc, idade);
+        gravados.add(texto);
+
+        //escrevendo no .txt
+        escrever(gravados);
     }
-    public String escrever(String nome,String saude,double peso, double altura, double imc, int idade){
-        String texto="Nome "+nome+"\n"+"Saude "+saude+"\n"+"peso "+peso+"\n"+"altura "+altura+"\n"+"IMC "+imc+"\n"+"idade "+idade;
-        return texto;
+    public List<String> verificandoSalvos() throws IOException{
+        List<String> gravados = Files.exists(caminhodoarquivo) ? new ArrayList<>(Files.readAllLines(caminhodoarquivo)) : new ArrayList<>();
+        // Adiciona cabeçalho se não existir
+        if (!gravados.contains(this.campos)) {
+            gravados.add(campos);
+        }
+        return gravados;
+    }
+
+    public String organizar(String nome, String saude, double peso, double altura, double imc, int idade) {
+        return nome + "; " + saude + "; " + peso + "; " + altura + "; " + imc + "; " + idade;
+    }
+    public void escrever(List<String> gravados) throws IOException{
+        // Reescreve o arquivo inteiro
+        Files.write(caminhodoarquivo, gravados, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 }
